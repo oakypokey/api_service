bc119d7f910b418eb54fbbbe359701fdd053f163
oakypokey <hello@calvindass.com>
2023-05-17 16:57:51 -0700

    fixed config for Docker

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from dotenv import load_dotenv
 
 from fastapi import FastAPI, HTTPException, Response, Depends
 
-from dependencies import get_query_token, get_token_header
-from routers import slack_bot
+from .dependencies import get_query_token, get_token_header
+from .routers import slack_bot
 
 app = FastAPI()
 app.include_router(slack_bot.router)

8bc3e2d81940fc7f6293bdc42f15c43e704815ae
oakypokey <hello@calvindass.com>
2023-05-18 10:07:51 -0700

    changing slack bot strat

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import json
 from dataclasses import dataclass, field
+from slack_bolt.adapter.socket_mode import SocketModeHandler
 
 from dotenv import load_dotenv
 
@@ -9,10 +10,12 @@ from src.dependencies import get_query_token, get_token_header
 from src.routers import slack_bot
 
 app = FastAPI()
-app.include_router(slack_bot.router)
 
 @app.get("/")
 def read_root() -> Response:
     return Response("The server is running!")
+
+if __name__ == "__main__":
+    SocketModeHandler(app, os.environ.get("SLACK_APP_TOKEN")).start()
 
 
--- src/routers/slack_bot.py
+++ src/routers/slack_bot.py
@@ -1,62 +1,19 @@
-from dotenv import load_dotenv
-from slack_sdk import WebClient
-from fastapi import APIRouter, Depends, Query
-from fastapi.responses import HTMLResponse
-from typing import Annotated
-
 import os
-from src.dependencies import get_token_header
 
-scopes = [
-    'chat:write',
-    'channels:read',
-    'app_mentions:read'
-]
-
-if(os.environ.get('ENV') == 'dev'):
-    URL_STUB = "http://localhost:8080"
-else:
-    URL_STUB = "https://apiservice-growthcopilot.b4a.run"
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+from dotenv import load_dotenv
 
 load_dotenv()
 
-SLACK_CLIENT = WebClient()
+# Install the Slack app and get xoxb- token in advance
+app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 
-router = APIRouter(prefix="/slack",
-    # dependencies=[Depends(get_token_header)],
-    responses={404: {"description": "Not a valid endpoint"}}
-)
+@app.command("/hello-socket-mode")
+def hello_command(ack, body):
+    user_id = body["user_id"]
+    ack(f"Hi, <@{user_id}>!")
 
-@router.get("/install")
-def pre_install():
-    url = URL_STUB + '/slack/oauth_redirect'
-    state = "12345"
-    scope_string = '&amp;'.join(scopes)
-    print(scope_string)
-    return HTMLResponse('<html><a href="https://slack.com/oauth/v2/authorize?' \
-        f'scope={scope_string}&client_id={os.environ.get("SLACK_CLIENT_ID")}&state={state}&redirect_uri={url}">' \
-        'Add to Slack</a></html>')
-
-
-@router.get("/oauth_redirect")
-def post_install(code: str | None, state: str | None ):
-    # Verify the "state" parameter
-
-    # Request the auth tokens from Slack
-    response = SLACK_CLIENT.oauth_v2_access(
-        client_id=os.environ.get("SLACK_CLIENT_ID"),
-        client_secret=os.environ.get("SLACK_CLIENT_SECRET"),
-        code=code
-    )
-    print(response)
-
-    # Save the bot token to an environmental variable or to your data store
-    # for later use
-    os.environ["SLACK_BOT_TOKEN"] = response['access_token']
-
-    # Don't forget to let the user know that OAuth has succeeded!
-    return "Installation is completed!"
-
-@router.post("/cheer")
-def assign_cheer():
-    return "cheer!"
+@app.event("app_mention")
+def event_test(say):
+    say("Hi there!")

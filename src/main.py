3d1dc88eecfab16ac3b87557156a7917096a5ded
oakypokey <hello@calvindass.com>
2023-05-17 17:23:57 -0700

    adjusting config for docker

---
+++ src/main.py
@@ -0,0 +1,18 @@
+import json
+from dataclasses import dataclass, field
+
+from dotenv import load_dotenv
+
+from fastapi import FastAPI, HTTPException, Response, Depends
+
+from src.dependencies import get_query_token, get_token_header
+from src.routers import slack_bot
+
+app = FastAPI()
+app.include_router(slack_bot.router)
+
+@app.get("/")
+def read_root() -> Response:
+    return Response("The server is running!")
+
+
---
+++ src/routers/slack_bot.py
@@ -0,0 +1,61 @@
+from dotenv import load_dotenv
+from slack_sdk import WebClient
+from fastapi import APIRouter, Depends, Query
+from fastapi.responses import HTMLResponse
+from typing import Annotated
+
+import os
+from src.dependencies import get_token_header
+
+scopes = [
+    'chat:write',
+    'channels:read',
+    'app_mentions:read'
+]
+
+load_dotenv()
+
+SLACK_CLIENT = WebClient()
+
+router = APIRouter(prefix="/slack",
+    # dependencies=[Depends(get_token_header)],
+    responses={404: {"description": "Not a valid endpoint"}}
+)
+
+@router.get("/install")
+def pre_install():
+    if(os.environ.get('ENV') == 'dev'):
+        url = "https://localhost:8080/slack/oauth_redirect"
+    else:
+        url = "https://apiservice-growthcopilot.b4a.run/slack/oauth_redirect"
+
+    state = "12345"
+    scope_string = '&amp;'.join(scopes)
+    print(scope_string)
+    return HTMLResponse('<html><a href="https://slack.com/oauth/v2/authorize?' \
+        f'scope={scope_string}&client_id={os.environ.get("SLACK_CLIENT_ID")}&state=12345&url={url}">' \
+        'Add to Slack</a></html>')
+
+
+@router.get("/oauth_redirect")
+def post_install(code: str | None, state: str | None ):
+    # Verify the "state" parameter
+
+    # Request the auth tokens from Slack
+    response = SLACK_CLIENT.oauth_v2_access(
+        client_id=os.get.environ("SLACK_CLIENT_ID"),
+        client_secret=os.get.environ("SLACK_CLIENT_SECRET"),
+        code=code
+    )
+    print(response)
+
+    # Save the bot token to an environmental variable or to your data store
+    # for later use
+    os.environ["SLACK_BOT_TOKEN"] = response['access_token']
+
+    # Don't forget to let the user know that OAuth has succeeded!
+    return "Installation is completed!"
+
+@router.post("/cheer")
+def assign_cheer():
+    return "cheer!"
